3e2eda81c32e6bc86769eb7feec8852c2388f992
Tommy Persson <tommy.persson@gmail.com>
2023-07-02 06:38:09 +0200

    Increate test coverage for UnitOfWork

--- db/src/test/kotlin/io/tpersson/ufw/db/IntegrationTest.kt
+++ db/src/test/kotlin/io/tpersson/ufw/db/IntegrationTest.kt
@@ -7,18 +7,19 @@ import io.tpersson.ufw.db.jdbc.toMap
 import io.tpersson.ufw.db.jdbc.useInTransaction
 import io.tpersson.ufw.db.unitofwork.UnitOfWorkFactoryImpl
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.lifecycle.Startables
 import org.testcontainers.utility.DockerImageName
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.fail
+import java.sql.Connection
+import java.sql.PreparedStatement
 import java.util.UUID
-import kotlin.test.fail
 
 internal class IntegrationTest {
 
-    companion object {
+    private companion object {
         @JvmStatic
         var postgres: PostgreSQLContainer<*> = PostgreSQLContainer(DockerImageName.parse("postgres:15"))
 
@@ -38,47 +39,135 @@ internal class IntegrationTest {
 
         init {
             Startables.deepStart(postgres).join()
+
+            dataSource.connection.useInTransaction {
+                it.prepareStatement(
+                    """
+                    CREATE TABLE test (id UUID NOT NULL PRIMARY KEY)
+                    """.trimIndent()
+                ).execute()
+            }
         }
+    }
+
+    private val testItem1 = UUID.fromString("8b00ce00-4523-412b-b1fe-98d4737bf991")
+    private val testItem2 = UUID.fromString("d20f1adc-c1cf-4fc7-8752-948eed61cd4e")
+
+    private val unitOfWorkFactory = UnitOfWorkFactoryImpl(ConnectionProviderImpl(dataSource))
+
+    @BeforeEach
+    fun setUp() {
+        deleteAllItems()
     }
 
     @Test
     fun `Basic test`(): Unit = runBlocking {
-        dataSource.connection.useInTransaction {
-            it.prepareStatement(
-                """
-                CREATE TABLE test (id UUID NOT NULL PRIMARY KEY)
-            """.trimIndent()
-            ).execute()
-        }
-
-        val unitOfWorkFactory = UnitOfWorkFactoryImpl(ConnectionProviderImpl(dataSource))
-
         val unitOfWork = unitOfWorkFactory.create()
 
-        val testId = UUID.fromString("8b00ce00-4523-412b-b1fe-98d4737bf991")
+        val testItem = UUID.fromString("8b00ce00-4523-412b-b1fe-98d4737bf991")
 
         unitOfWork.add {
-            prepareStatement(
-                """
-                INSERT INTO test (id) VALUES (?)
-            """.trimIndent()
-            ).also {
-                it.setObject(1, testId)
-            }
+            insertItem(testItem)
         }
 
-        assertThat(doesIdExistInDb(testId)).isFalse()
+        assertThat(doesItemExist(testItem)).isFalse()
 
         unitOfWork.commit()
 
-        assertThat(doesIdExistInDb(testId)).isTrue()
+        assertThat(doesItemExist(testItem)).isTrue()
     }
 
-    private fun doesIdExistInDb(id: UUID): Boolean {
-        val result = dataSource.connection.use {
-            it.prepareStatement("SELECT * FROM test").executeQuery().toMap()
+    @Test
+    fun `Multiple items in UnitOfWork`(): Unit = runBlocking {
+        val unitOfWork = unitOfWorkFactory.create()
+
+        unitOfWork.add {
+            insertItem(testItem1)
         }
 
-        return result.any { it.get("id") == id }
+        unitOfWork.add {
+            insertItem(testItem2)
+        }
+
+        unitOfWork.commit()
+
+        assertThat(doesItemExist(testItem1)).isTrue()
+        assertThat(doesItemExist(testItem2)).isTrue()
+    }
+
+    @Test
+    fun `Error in UnitOfWork item prevents commit`(): Unit = runBlocking {
+        val unitOfWork = unitOfWorkFactory.create()
+
+        unitOfWork.add {
+            insertItem(testItem1)
+        }
+
+        unitOfWork.add {
+            insertItem(testItem2)
+        }
+
+        unitOfWork.add {
+            error("Oh, no")
+        }
+
+        assertThatThrownBy { runBlocking { unitOfWork.commit() } }.isNotNull()
+
+        assertThat(doesItemExist(testItem1)).isFalse()
+        assertThat(doesItemExist(testItem2)).isFalse()
+    }
+
+    @Test
+    fun `Failed 'minimumAffectedRows' check prevents commit`(): Unit = runBlocking {
+        val unitOfWork = unitOfWorkFactory.create()
+
+        unitOfWork.add(minimumAffectedRows = 2) {
+            insertItem(testItem1)
+        }
+
+        assertThatThrownBy { runBlocking { unitOfWork.commit() } }.isNotNull()
+
+        assertThat(doesItemExist(testItem1)).isFalse()
+    }
+
+    @Test
+    fun `'minimumAffectedRows' can be '0'`(): Unit = runBlocking {
+        val unitOfWork = unitOfWorkFactory.create()
+
+        unitOfWork.add(minimumAffectedRows = 1) {
+            insertItem(testItem1)
+        }
+
+        unitOfWork.add(minimumAffectedRows = 0) {
+            insertItem(testItem1)
+        }
+
+        unitOfWork.commit()
+
+        assertThat(doesItemExist(testItem1)).isTrue()
+    }
+
+    private fun deleteAllItems() {
+        dataSource.connection.useInTransaction {
+            it.prepareStatement("DELETE FROM test").execute()
+        }
+    }
+
+    private fun Connection.insertItem(item: UUID): PreparedStatement {
+        return prepareStatement(
+            """
+            INSERT INTO test (id) VALUES (?) ON CONFLICT DO NOTHING
+            """.trimIndent()
+        ).also {
+            it.setObject(1, item)
+        }
+    }
+
+    private fun doesItemExist(id: UUID): Boolean {
+        val result = dataSource.connection.use {
+            it.prepareStatement("SELECT * FROM test WHERE id = '$id'").executeQuery().toMap()
+        }
+
+        return result.any()
     }
 }
