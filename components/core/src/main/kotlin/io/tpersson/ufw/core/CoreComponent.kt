33aeebb1e7f0f74d174e3577e1deb54fc4df0b6e
Tommy Persson <tommy.persson@gmail.com>
2023-07-09 22:10:12 +0200

    Configure Jackson to serialize time-related objects as ISO-8601

--- components/core/src/main/kotlin/io/tpersson/ufw/core/CoreComponent.kt
+++ components/core/src/main/kotlin/io/tpersson/ufw/core/CoreComponent.kt
@@ -2,6 +2,7 @@ package io.tpersson.ufw.core
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
@@ -24,6 +25,7 @@ public class CoreComponent @Inject private constructor(
 
         public val defaultObjectMapper: ObjectMapper = jacksonObjectMapper().findAndRegisterModules().also {
             it.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            it.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
     }
 }
---
+++ components/core/src/test/kotlin/io/tpersson/ufw/core/ObjectMapperTest.kt
@@ -0,0 +1,24 @@
+package io.tpersson.ufw.core
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.Instant
+
+internal class ObjectMapperTest {
+    @Test
+    fun `Can serialize and deserialize Instants as ISO-8601`() {
+        val objectMapper = CoreComponent.defaultObjectMapper
+
+        val expectedString = "2020-02-02T12:02:02.002Z"
+        val expectedInstant = Instant.parse(expectedString)
+
+        val actual = objectMapper.writeValueAsString(expectedInstant)
+
+        assertThat(actual).isEqualTo("\"$expectedString\"")
+
+        val deserialized = objectMapper.readValue<Instant>(actual)
+        
+        assertThat(deserialized).isEqualTo(expectedInstant)
+    }
+}
