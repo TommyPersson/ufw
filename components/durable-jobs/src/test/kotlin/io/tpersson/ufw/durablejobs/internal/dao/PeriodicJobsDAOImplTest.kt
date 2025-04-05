20dcedfb5710224914cab8c982b5de45e1e7c25c
Tommy Persson <tommy.persson@gmail.com>
2025-04-05 17:03:17 +0200

    Add tests for PeriodicJobsDAOImpl

---
+++ components/durable-jobs/src/test/kotlin/io/tpersson/ufw/durablejobs/internal/dao/PeriodicJobsDAOImplTest.kt
@@ -0,0 +1,111 @@
+package io.tpersson.ufw.durablejobs.internal.dao
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.tpersson.ufw.core.dsl.UFW
+import io.tpersson.ufw.core.dsl.core
+import io.tpersson.ufw.database.dsl.database
+import io.tpersson.ufw.databasequeue.dsl.databaseQueue
+import io.tpersson.ufw.durablejobs.DurableJobQueueId
+import io.tpersson.ufw.durablejobs.dsl.durableJobs
+import io.tpersson.ufw.managed.dsl.managed
+import io.tpersson.ufw.test.TestInstantSource
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.lifecycle.Startables
+import org.testcontainers.utility.DockerImageName
+import java.time.Duration
+
+internal class PeriodicJobsDAOImplTest {
+    companion object {
+        @JvmStatic
+        var postgres: PostgreSQLContainer<*> = PostgreSQLContainer(DockerImageName.parse("postgres:15"))
+            .withReuse(true)
+            .also {
+                Startables.deepStart(it).join()
+            }
+
+        val config = HikariConfig().also {
+            it.jdbcUrl = postgres.jdbcUrl
+            it.username = postgres.username
+            it.password = postgres.password
+            it.maximumPoolSize = 5
+            it.isAutoCommit = false
+        }
+
+        val testClock = TestInstantSource()
+
+        val ufw = UFW.build {
+            core {
+                clock = testClock
+            }
+            managed {
+            }
+            database {
+                dataSource = HikariDataSource(config)
+            }
+            databaseQueue {
+            }
+            durableJobs {
+            }
+        }
+
+        val database = ufw.database.database
+        val unitOfWorkFactory = ufw.database.unitOfWorkFactory
+
+        init {
+            ufw.database.migrator.run()
+        }
+    }
+
+    private lateinit var dao: PeriodicJobsDAOImpl
+
+    @BeforeEach
+    fun setUp() {
+        dao = PeriodicJobsDAOImpl(database)
+    }
+
+    @Test
+    fun `get - Returns null for missing data`(): Unit = runBlocking {
+        val queueId = DurableJobQueueId("unknown")
+        val jobType = "unknown"
+
+        val result = dao.get(queueId, jobType)
+
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun `put - Inserts data correctly`(): Unit = runBlocking {
+        val queueId = DurableJobQueueId("unknown")
+        val jobType = "unknown"
+
+        val lastSchedulingAttempt = testClock.dbNow
+        testClock.advance(Duration.ofMinutes(1))
+        val nextSchedulingAttempt = testClock.dbNow
+
+        val unitOfWork = unitOfWorkFactory.create()
+
+        dao.put(
+            queueId = queueId,
+            jobType = jobType,
+            state = PeriodicJobStateData(
+                queueId = queueId.value,
+                jobType = jobType,
+                lastSchedulingAttempt = lastSchedulingAttempt,
+                nextSchedulingAttempt = nextSchedulingAttempt,
+            ),
+            unitOfWork = unitOfWork,
+        )
+
+        unitOfWork.commit()
+
+        val result = dao.get(queueId, jobType)
+
+        assertThat(result?.lastSchedulingAttempt).isEqualTo(lastSchedulingAttempt)
+        assertThat(result?.nextSchedulingAttempt).isEqualTo(nextSchedulingAttempt)
+    }
+}
